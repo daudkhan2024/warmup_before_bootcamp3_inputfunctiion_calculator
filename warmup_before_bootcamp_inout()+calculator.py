2e93a189585bdd66a38086adef2835f1e1b4e566
Daud Khan <109561961+daudkhan2024@users.noreply.github.com>
2023-01-22 14:44:58 +0100

    Add files via upload
    Here you will see how the input() work and I make also a simple calculator 
    and difference between int() and float() you will notice ..Explained

---
+++ warmup_before_bootcamp_inout()+calculator.py
@@ -0,0 +1,16 @@
+# Getting input from Users
+name=input("Enter your name :")
+age = input("Please! Enter your age :")
+print(" Hello "+ name +" !, Your age is " + age +"")
+
+# Making Cauculator
+Num1 = input("Enter a number")
+Num2 = input("Enter other number")
+Result = int(Num1) + int(Num2)
+print(Result)
+
+# For Float or any kinda number Float is better
+Num1 = input("Enter a number")
+Num2 = input("Enter other number")
+Result = float(Num1) + float(Num2)
+print(Result)
